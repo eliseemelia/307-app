bb5af8b3f445597b818c9919f1dde4268f5e7591
eliseemelia <eliseemelia@gmail.com>
2022-09-30 12:35:25 -0700

    fixed folders

---
+++ src/MyApp.js
@@ -0,0 +1,78 @@
+
+import React, {useState, useEffect} from 'react'
+import Table from './Table'
+import Form from './Form'
+import axios from 'axios';
+
+function MyApp() {
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    fetchAll().then( result => {
+      if (result) 
+        setCharacters(result);
+    });
+  }, [] );
+  
+  function removeOneCharacter (index, id) {
+    makeDeleteCall(id).then( result => {
+      if (result && result.status === 204){
+        const updated = characters.filter((character, i) => {
+          return i !== index
+        });
+        setCharacters(updated);
+      }
+    });
+  }
+
+  async function makeDeleteCall(id) {
+    try {
+      const response = await axios.delete('http://localhost:5000/users/${id}');
+      return response;
+    }
+    catch (error){
+      console.log(error);
+      return false;
+    }
+  }
+
+  function updateList(person) {
+    makePostCall(person).then( result => {
+      if (result && result.status === 201)
+        setCharacters([...characters, result.data]);
+    });
+  }
+
+  async function fetchAll(){
+    try {
+      const response = await axios.get('http://localhost:5000/users');
+      return response.data.users_list;
+    }
+    catch (error){
+      console.log(error);
+      return false;
+    }
+  }
+
+  async function  makePostCall(person) {
+    try {
+      const response = await axios.post('http://localhost:5000/users', person);
+      return response;
+    }
+    catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  return (
+    <div className='container'>
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
+    </div>
+  );
+}
+
+  
+
+
+  export default MyApp;
